fed92a574a97e7d1bfadae3d968a3477784af4a7
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-15 22:43:33 -0500

    ./H.Meneses

--- Linux-System/configs/exec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# - Certifique-se de tornar o script executável usando o comando 
-# - chmod +x nome_do_script.sh e, em seguida, execute-o com 
-# - ./nome_do_script.sh. Certifique-se também de que o Bash está instalado no seu sistema.
